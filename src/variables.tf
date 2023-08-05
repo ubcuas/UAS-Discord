b40568dd18a2083d51ab8ab12227366823a5fe54
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-08-05 16:16:20 -0700

    Add UAS Discord bot ID variable

--- src/variables.tf
+++ src/variables.tf
@@ -1,29 +1,35 @@
 variable "terraform_bot_token" {
-    type        = string
-    description = "The token for the Terraform Discord bot"
-    sensitive   = true
+  type        = string
+  description = "The token for the Terraform Discord bot"
+  sensitive   = true
 }
 
 variable "terraform_bot_id" {
-    type        = string
-    description = "The ID of the Terraform Discord bot"
-    sensitive   = true
+  type        = string
+  description = "The ID of the Terraform Discord bot"
+  sensitive   = true
+}
+
+variable "uas_bot_id" {
+  type        = string
+  description = "The ID of the UAS Discord bot"
+  sensitive   = true
 }
 
 variable "server_id" {
-    type        = string
-    description = "The ID of the UAS Discord server"
-    sensitive   = true
+  type        = string
+  description = "The ID of the UAS Discord server"
+  sensitive   = true
 }
 
 variable "uas_account_id" {
-    type        = string
-    description = "The ID of the UAS admin Discord account"
-    sensitive   = true
+  type        = string
+  description = "The ID of the UAS admin Discord account"
+  sensitive   = true
 }
 
 variable "github_token" {
-    type        = string
-    description = "The token for the GitHub provider"
-    sensitive   = true
+  type        = string
+  description = "The token for the GitHub provider"
+  sensitive   = true
 }
