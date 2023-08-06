ca5acea06a7a4844d4492e49716d9b8dc068fa5a
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-08-06 11:51:42 -0700

    Message formatting changes

--- src/messages.tf
+++ src/messages.tf
@@ -2,7 +2,7 @@ resource "discord_message" "image" {
   channel_id = discord_text_channel.about.id
 
   embed {
-    color       = data.discord_color.uas_blue.dec
+    color = data.discord_color.uas_blue.dec
     image {
       url = "https://raw.githubusercontent.com/ubcuas/UAS-Discord/master/static/logo.png"
     }
@@ -15,15 +15,17 @@ resource "discord_message" "server_info" {
 
   embed {
     title       = "Server Information"
+    color       = data.discord_color.uas_blue.dec
     description = <<-EOT
       Welcome to the UBC UAS Discord server!
+
     EOT
-    color       = data.discord_color.uas_blue.dec
 
     fields {
       name   = "Roles"
+      inline = false
       value  = <<-EOT
-        We have a variety of roles for oranization purposes which our managed by our <@${var.uas_bot_id}>. If you have an issue with permissions please reach out to the admin team.
+        We have a variety of roles for oranization purposes which our managed by the <@${var.uas_bot_id}>. If you have an issue with permissions please reach out to the admin team.
 
         - <@&${discord_role.captain.id}>: Captain
         - <@&${discord_role.sub-team_lead.id}>: Sub-team leads
@@ -35,12 +37,13 @@ resource "discord_message" "server_info" {
         - <@&${discord_role.verified.id}>: All current members who have verified
         - <@&${discord_role.uas_alumni.id}>: Past members of the team
         - @everyone: Default role for all members (minimal permissions beyond seeing the verify channel)
+
       EOT
-      inline = false
     }
 
     fields {
       name   = "Categories & Channels"
+      inline = false
       value  = <<-EOT
         The server is structured into a few key categories with corresponding channels. The purpose of each channel should be clear by its name but you can also check its description to be sure.
 
@@ -50,21 +53,21 @@ resource "discord_message" "server_info" {
         - ${discord_category_channel.general.name}: General discussion and voice channels
         - ${discord_category_channel.specialized.name}: Topic-specific channels
         - Sub-Team Categories: Categories for each sub-team with their own channels
+
       EOT
-      inline = false
     }
 
     fields {
       name   = "FAQ"
+      inline = false
       value  = <<-EOT
-        **How can I request and/or make server changes (e.g. new channels, roles, permission changes, etc.)?**
+        How can I request and/or make server changes (e.g. new channels, roles, permission changes, etc.)?
         - To request a change, create an issue [here](https://github.com/ubcuas/UAS-Discord/issues/new/choose) - this requires a GitHub account
         - To make a change, create a pull request on GitHub [here](https://github.com/ubcuas/UAS-Discord/)
 
-        **Where can I find the invite link to the server?**
+        Where can I find the invite link to the server?
         - https://discord.gg/${discord_invite.main.id}
       EOT
-      inline = false
     }
   }
 }
@@ -75,17 +78,24 @@ resource "discord_message" "rules" {
 
   embed {
     title       = "Rules"
-    description = "UAS hopes to foster a welcoming and tight-knit community. To ensure this, all members must strictly adhere to our rules."
     color       = data.discord_color.uas_blue.dec
+    description = <<-EOT
+      UAS hopes to foster a welcoming and tight-knit community. To ensure this, all members must strictly adhere to our rules.
+
+    EOT
 
     fields {
       name   = "Member Agreement"
-      value  = "By joining UBC UAS you agree to our [rules and guidelines](https://screenmessage.com/uas-agreement-wip)."
       inline = false
-    }
+      value  = <<-EOT
+        By joining UBC UAS you agree to our [rules and guidelines](https://screenmessage.com/uas-agreement-wip).
+
+      EOT
+}
 
     fields {
       name   = "Discord Rules"
+      inline = false
       value  = <<-EOT
         1. Be respectful and appropriate
         Our goal is to create a tight-knit community where everyone is welcome and kind to each other. To achieve this, there will absolutely be no sexual content, homophobia, racism, transphobia, or body shaming of any of kind.
@@ -98,16 +108,16 @@ resource "discord_message" "rules" {
 
         4. Discord's TOS and Community Guidelines
         You must abide by the rules noted in Discord's [Terms of Service](https://discord.com/terms) and [Community Guidelines](https://discord.com/guidelines).
+
       EOT
-      inline = false
     }
 
     fields {
       name   = "Report an Issue"
+      inline = false
       value  = <<-EOT
         We work hard to make the UAS community a safe place for everyone but acknowledge that this isn't always possible. If you have an issue with anyone at any time and don't feel comfortable talking to them directly, please reach out to either the captain or one of the leads (whoever you feel most comfortable talking to).
       EOT
-      inline = false
     }
   }
 }
@@ -118,9 +128,9 @@ resource "discord_message" "resources" {
 
   embed {
     title       = "Team Resources"
+    color       = data.discord_color.uas_blue.dec
     description = <<-EOT
       UAS operates on a variety of platforms to take advantage of the best tools for each task. To find all of these tools and resources please visit the [Confluence home page](https://ubcuas.atlassian.net/wiki).
     EOT
-    color       = data.discord_color.uas_blue.dec
   }
 }
